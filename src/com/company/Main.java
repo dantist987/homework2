b5dc57caf48f9d01bde0d579e5a759831a9e9bf0
Adilet <adik.isak2412@gmail.com>
2022-02-20 22:13:25 +0600

    added randomization generating method.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,47 @@
 package com.company;
 
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
+        int age1 = generateRandomAge();
+        System.out.println(res(age1, 12));
 
-        System.out.println(res(18, 12));
-        System.out.println(res(32, -8));
-        System.out.println(res(46, -7));
-        System.out.println(res(50, 50));
-        System.out.println(res(20, 12));
+        int age2 = generateRandomAge();
+        System.out.println(res(age2, -8));
+
+        int age3 = generateRandomAge();
+        System.out.println(res(age3, -7));
+
+        int age4 = generateRandomAge();
+        System.out.println(res(age4, 50));
+
+        int age5 = generateRandomAge();
+        System.out.println(res(age5, 12));
     }
+
     public static String res(int age, int temp) {
         if (age >= 20 && age < 45 && temp > -20 && temp < 30) {
-            return "Можно идти гулять. ";
+            return "Возраст " + age + " Можно идти гулять. ";
         } else if (age < 20 && temp > 0 && temp < 28) {
-            return "Нужно идти гулять. ";
+            return "Возраст " + age + " Нужно идти гулять. ";
         } else if (age > 45 && temp > -10 && temp < 25) {
-            return "Подумайте, прежде чем выйти на улицу. ";
+            return"Возраст " + age + " Подумайте, прежде чем выйти на улицу. ";
         } else {
-            return "Оставайтесь дома. ";
+            return"Возраст " + age + " Оставайтесь дома. ";
         }
     }
+
+    public static int generateRandomAge(){
+        Random random = new Random();
+        int age = random.nextInt(100);
+        return age;
+    }
+
 }
 
 
